be6772817bb69ffd629d642e25614d1c1036d542
Trần Quang Huy <quanghuy7927@gmail.com>
2023-03-02 16:02:11 +0700

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Server 2
+          Server 3
         </p>
       </header>
     </div>
